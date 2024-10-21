ab1caf88d7faa345bd0e7f24594c28c68694388e
Jeff555max <vadik98765@gmail.com>
2024-10-21 17:52:07 +0500

    Update main.py
    убран токен

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 import time
 import threading
 
-API_TOKEN = '7945599886:AAEgoIJEkhgoy-ltY5bYbGaGlb3oiEx9uA8'
+API_TOKEN = 'Введите Ваш токен'
 bot = telebot.TeleBot(API_TOKEN)
 
 # Словарь для хранения времени напоминаний каждого пользователя
